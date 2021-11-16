cf09926c3ebed06db0e8dd5e4fb323b963dfefb9
maxmustermensch <mvst1990@gmail.com>
2021-11-16 23:23:36 +0100

    adding function_testing_2.py with alternative stepper motor controlling

--- function_testing.py
+++ function_testing.py
@@ -1,2 +1,16 @@
-import Jetson.GPIO
-import RpiMotorLib
+import Jetson.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+
+GPIO_pins = (12, 8, 4)
+direction = 20
+step = 21
+
+mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "DRV8825")
+
+j = False
+
+for i in range(0,100,1):
+    mymotortest.motor_go(j, "Full", 500, .001, False, .05)
+    j = not j
+
+GPIO.cleanup

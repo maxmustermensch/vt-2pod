13aeffa5b3daade4646f4817dfc608d366537ccd
maxmustermensch <mvst1990@gmail.com>
2021-12-02 17:14:48 +0100

    added functional testing and burst to vt-2pod

--- vt-2pod.py
+++ vt-2pod.py
@@ -9,30 +9,38 @@
 
 #IMPORTS_______________________________________________________________
 import Jetson.GPIO as GPIO
+import numpy as np
 import os
 import time
 from RpiMotorLib import RpiMotorLib
 
 #GLOBAL_VARIABLES______________________________________________________
 
-#define GPIOs
+GPIO.setmode(GPIO.BCM)
+
+#define GPIOs stepper
 PINS_mode = (8, 7, 11)
 PIN_dir = 20
 PIN_step = 21
 PIN_ibutt = 6
 PIN_stepper_sleep = 24
 
+#define GPIOs vibration motors
+PIN_center = 26
+PIN_white = 19
+PIN_yellow = 13
+
+GPIO.setup([PIN_center, PIN_white, PIN_yellow], GPIO.OUT)
 
 #define init button
-GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_ibutt, GPIO.IN)
 GPIO.setup(PIN_stepper_sleep, GPIO.OUT)
 
 
 #define stepper
 speed = 2000
-mode = "1/32"
-modedic = {
+stp_mode = "1/32"
+stp_mode_dic = {
     "Full": 1,
     "Half": 2,
     "1/4": 4,
@@ -40,8 +48,8 @@ modedic = {
     "1/16": 16,
     "1/32": 32
     }
-fac = modedic[mode]
-stpsis = 0
+fac = stp_mode_dic[stp_mode]
+stps_is = 0
 dir = False     #False -> closing arms / True -> open
 
 stepper = RpiMotorLib.A4988Nema(PIN_dir, PIN_step, PINS_mode, "DRV8825")
@@ -56,13 +64,30 @@ GPIO.add_event_detect(PIN_ibutt, GPIO.RISING, callback = interrupt_service_routi
 
 #FUNCTIONS ESSENTIAL____________________________________________________
 
-def choose_test_mode(mode):
+def testing():
     '''
     IN:
     OUT:
     DO:
     - USR sucht den Testmodus aus
     '''
+    test_mode_dic = {
+        "0": [3, [40, 20, 12]],
+        "1": [7, [40, 32, 24, 20, 16, 14, 12]],
+        "2": [7, [40, 36, 32, 28, 20, 12]]
+
+        }
+
+    print("    0: user training \n    1: forearm \n    2: thigh\n")
+    test_mode = input("choose test mode:")
+    test_arr = test_mode_dic[test_mode]
+
+    for m in test_arr[1]:
+        for n in range(0,test_arr[0],1):
+            get_pos(m)
+
+            print("postition ", m,"mm - burst #", n+1, sep="")
+
 
 
 def home_pos():
@@ -73,7 +98,7 @@ def home_pos():
     - faehrt den unteren totpunkt (ut)
     - > kalibiriert stellweg auf null
     '''
-    global stpsis
+    global stps_is
     dir = False
     stps_home_dist = 12   #distance steps back from end stop to 0-position (12mm)
 
@@ -83,18 +108,18 @@ def home_pos():
     if GPIO.input(PIN_ibutt) == 0:
         time.sleep (0.005)
         if GPIO.input(PIN_ibutt)== 0:
-            stepper.motor_go(dir, mode, 1000*fac, 1/fac/speed, False, 0.05)
+            stepper.motor_go(dir, stp_mode, 1000*fac, 1/fac/speed, False, 0.05)
     else:
-        stepper.motor_go(not dir, mode, 10*fac, 1/fac/speed, False, 0.05)
-        stepper.motor_go(dir, mode, 15*fac, 1/fac/speed, False, 0.05)
+        stepper.motor_go(not dir, stp_mode, 10*fac, 1/fac/speed, False, 0.05)
+        stepper.motor_go(dir, stp_mode, 15*fac, 1/fac/speed, False, 0.05)
 
 
     time.sleep(0.2)
-    stepper.motor_go(not dir, mode, stps_home_dist*fac, 1/fac/speed, False, 0.05)
+    stepper.motor_go(not dir,stp_mode, stps_home_dist*fac, 1/fac/speed, False, 0.05)
 
-    stpsis = 0
+    stps_is = 0
 
-    #stepper.motor_go(not dir, mode, 380*fac, 1/fac/speed, False, 0.05)
+    #stepper.motor_go(not dir, stp_mode, 380*fac, 1/fac/speed, False, 0.05)
 
     return
 
@@ -109,41 +134,63 @@ def get_pos(dist):
 
     '''
 
-    global stpsis 
+    global stps_is 
     l = 90          #lenght lever
     dCB = -13       #
-    stpspmm = 25    #steps per mm
+    stps_p_mm = 25    #steps per mm
     d0 = 12         #x-axis motor distance at 0-position
+    dmax = 45       #maximum x-axis motor distance
     h0 = 86.458     #z-axis joint distance at 0-position
 
     if dist < d0:
         print("Error: value lower 0-position")
         return
 
-    stpsgoal = int(-((l**2-((dist)-dCB)**2)**0.5-h0)*stpspmm)
+    if dist > dmax:
+        print("Error: value higher maximum")
+        return
+
+    stps_goal = int(-((l**2-((dist)-dCB)**2)**0.5-h0)*stps_p_mm)
     
     
-    if stpsgoal-stpsis > 0:
+    if stps_goal-stps_is > 0:
         dir = True
     else:  
         dir = False
 
-    stepper.motor_go(dir, mode, abs(stpsgoal-stpsis)*fac, 1/fac/speed, False, 0.05)
-    stpsis = stpsgoal
-    print(stpsis)
+    stepper.motor_go(dir, stp_mode, abs(stps_goal-stps_is)*fac, 1/fac/speed, False, 0.05)
+    stps_is = stps_goal
+    
 
     return
 
 
-def puls_pattern():
+def burst(v_motor_index, burst_index):
     '''
     IN: auswahl motoren; auswahl des gewuenschten pulsmusters
     OUT:
     DO:
     - steuert die motoren im geforderten pulsmuster an
     
-    
     '''
+
+    burst_beat = 0.05
+
+    burst_dic = {
+        "0": np.array([1, 1, 1, 1, 1, 1, 1, 1]),
+        "1": np.array([1, 0, 1, 0, 1, 0, 1, 0])
+    }
+
+    burst_mode = burst_dic[burst_index]
+
+    for i in burst_mode:
+        motor_arr = i*v_motor_index
+        GPIO.output([PIN_center, PIN_white, PIN_yellow], motor_arr)
+        time.sleep(burst_beat)
+
+    return
+
+
 
 def init_tsi():
     '''
@@ -154,6 +201,8 @@ def init_tsi():
     - stellt sicher, dass fernbedienung funktioniert
     
     '''
+
+
 
 #FUNKCIONENS OPTIONAL___________________________________________________
 def acc():
@@ -169,14 +218,13 @@ def acc():
 #MAIN___________________________________________________________________
 def main():
     home_pos()
-    time.sleep(1)
-    get_pos(28)
-    time.sleep(1)
-    get_pos(14)
-    time.sleep(1)
-    get_pos(38)
-    time.sleep(1)
-    get_pos(20)
+    get_pos(30)
+    burst(np.array([1,1,0]),"1")
+    burst(np.array([1,0,1]),"0")
+    burst(np.array([0,1,0]),"1")
+    burst(np.array([0,0,1]),"1")
+    burst(np.array([1,0,0]),"1")
+ 
 
     GPIO.output(PIN_stepper_sleep, GPIO.LOW)
     GPIO.cleanup()
